b7dcc6708fa5d6779e1bb257931f2944c3f60c58
plentiz <80513312+plentiz@users.noreply.github.com>
2021-03-17 20:21:00 +0100

    Update PrintNumbersTest.java
    Correct indentation after VS-code misbehaving..

--- src/test/java/io/github/joaomlneto/travis_ci_tutorial_java/PrintNumbersTest.java
+++ src/test/java/io/github/joaomlneto/travis_ci_tutorial_java/PrintNumbersTest.java
@@ -11,40 +11,40 @@ import org.junit.Test;
 
 public class PrintNumbersTest {
 
-	private final PrintStream standardOut = System.out;
-	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
-	@Before
-	public void setUp() {
-		System.setOut(new PrintStream(outputStreamCaptor));
-	}
+    @Before
+    public void setUp() {
+	System.setOut(new PrintStream(outputStreamCaptor));
+    }
 
-	@Test
-    	public void printNumbers_10() {
-		PrintNumbers.printNumbersInRange(10);
-		assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9, 10", outputStreamCaptor.toString().trim());
-	}
+    @Test 
+    public void printNumbers_10() {
+	PrintNumbers.printNumbersInRange(10);
+	assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9, 10", outputStreamCaptor.toString().trim());
+    }
 
-	@Test
-    	public void printNumbers_11() {
-		PrintNumbers.printNumbersInRange(11);
-		assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11", outputStreamCaptor.toString().trim());
-	}
+    @Test
+    public void printNumbers_11() {
+	PrintNumbers.printNumbersInRange(11);
+	assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11", outputStreamCaptor.toString().trim());
+    }
 
-	@Test
-    	public void printNumbers_1() {
-		PrintNumbers.printNumbersInRange(1);
-		assertEquals("1", outputStreamCaptor.toString().trim());
-	}
+    @Test
+    public void printNumbers_1() {
+	PrintNumbers.printNumbersInRange(1);
+	assertEquals("1", outputStreamCaptor.toString().trim());
+    }
 
-	@Test
-    	public void printNumbers_0() {
-		PrintNumbers.printNumbersInRange(0);
-		assertEquals("", outputStreamCaptor.toString().trim());
-	}
+    @Test
+    public void printNumbers_0() {
+	PrintNumbers.printNumbersInRange(0);
+	assertEquals("", outputStreamCaptor.toString().trim());
+    }
 
-	@After
-	public void tearDown() {
-		System.setOut(standardOut);
-	}
+    @After
+    public void tearDown() {
+	System.setOut(standardOut);
+    }
 }
